4910987f45ae946f7407b930df7ffe2fe5e52222
Geoffrey Revis <gporev@gmail.com>
2016-03-04 16:36:50 -0600

    Adding multiple users

--- authentication.py
+++ authentication.py
@@ -11,14 +11,31 @@ def addUser():
     else:
         print('Adding new username...')
         admins[newAdmin] = newPass
-        global currentUser
-        currentUser = newAdmin
-        print('Hello ', newAdmin)
-        print('What would you like to do today?')
-        main()
-        # print(admins)
+        
 
-
+def addMultipleUsers():
+  number = input("How many users do you want to enter: ")
+  number = int(number)
+  x = 0
+  while(x < number):
+    addUser()
+    x += 1
+    
+def addMultipleOneInput():
+  users = input("Enter users following the convention: Username1,Password1|Username2,Password2")
+  #Username1,Password1|Username2,Password2
+  users_arr = users.split('|')
+  #["Username1,Password1", "Username2,Password2"]
+  for user in users_arr:
+    #"Username1,Password1"
+    user_arr = user.split(',')
+    #["Usernam1","Password1"]
+    admins[user_arr[0]] = user_arr[1]
+  
+def printUsers():
+  for kv in admins.items():
+    print(kv)
+  
 def removeUser():
     nameToremove = input('What user do you want to remove? Username: ')
     if nameToremove in admins:
@@ -32,13 +49,22 @@ def removeUser():
     print(admins)
 
 def authenticate():
-  inpt1 = input('Log In [ 1 ] Add a User [ 2 ]: ')
+  inpt1 = input('Log In [ 1 ] Add a User [ 2 ] Add Multiple Users [ 3 ] Add Multiple Users on one line [ 4 ] show users [ 5 ]: ')
   if(inpt1) == '1':
     if logIn():
       return True
   elif(inpt1 == '2'):
     if addUser():
       return True
+  elif(inpt1 == '3'):
+    addMultipleUsers()
+    authenticate()
+  elif(inpt1 == '4'):
+    addMultipleOneInput()
+    authenticate()
+  elif(inpt1 == '5'):
+    printUsers()
+    authenticate()
     
             
 def logIn():
